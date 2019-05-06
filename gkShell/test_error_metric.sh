e2979af6eb7c715c1ff149d273527ed95bbfe346
Boly38 <boly38@gmail.com>
2019-05-06 13:39:21 +0200

    clean old tests

--- gkShell/test_error_metric.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cat <<EOF | curl --data-binary @- http://gk:9091/metrics/job/errory
-# TYPE errory_metric counter
-# HELP errory_metric Our user encounter an error.
-errory_metric{level="warn"} 1
-EOF
