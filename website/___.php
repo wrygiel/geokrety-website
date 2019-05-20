6cbf4cece0408f4bd878a8f5b361513b050fe608
Vandeputte Brice <boly38@gmail.com>
2019-05-20 21:02:19 +0200

    links

--- website/___.php
+++ website/___.php
@@ -29,37 +29,15 @@ function objectToHtml($var) {
 
 <h2>Todo</h2>
 <ul>
- <li>create unit tests (mock http client?)</li>
+ <li>spec - https://github.com/geokrety/geokrety-website/wiki/Study-GKMConsistencyDesign </li>
+ <li>DONE - create unit tests (mock http client?)</li>
  <li>create integration tests : env var to enable them</li>
- <li>move GkmClient into a create dedicated repository : geokrety/gkmClient</li>
+ <li>DONE move GkmClient into a create dedicated repository : https://github.com/GeoKretyMap/php-client</li>
  <li>use home-made template to define fields to use as comparator</li>
  <li>first version append to a file geokrety that are not sync</li>
  <li>improved version that append to a redis queue or database geokrety that are not sync</li>
  <li>create another job that take this entries to trigger gkm/.../dirty api cf issue #324
 </ul>
-
-<pre>
-SyncState: could be null or in temp cache file on disk
-- rollId : current roll identifier, from 10_000 to 19_999. increment by 1 when timestamp is <= 0. move to rollId min value if out of range
-- timestamp : define the current geokrety offset to use (geokrety creation date). When <= 0 need to restart from scratch (last created geokrety as first batch)
-
-
-SyncParameters:
-- job startup trigger (cron entry config)
-- job max duration seconds
-- batch size (geokrety select limit)
-- roll min interval day
-
-
-started by cron configuration a given job must finish the current roll in the limit of max duration
-when the roll is finished, there's no more action to do
-a roll is defined by the check of all geokrety table entries
-we start by using current datetime and a select of X geokrety order by creation date desc
-X is max batch size
-
-
-if a job is triggered with an ended roll, a new roll is started if period between roll is reached
-</pre>
 
 
 
